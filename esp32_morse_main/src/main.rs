c3ee7b3979dfc5ac8b1eb7c285e5185d500b1d48
Andrei Dodu <andrei.dodu@hotmail.it>
2023-11-19 00:10:56 +0100

    chore

--- esp32_morse_main/src/main.rs
+++ esp32_morse_main/src/main.rs
@@ -32,7 +32,7 @@ fn main() -> anyhow::Result<()> {
 fn print_message(morse: &Vec<u32>) {
     println!("Sending: {}", MESSAGE);
     println!("Translated ");
-    for morse_value in morse.clone() {
+    for morse_value in morse.to_owned().clone() {
         if morse_value == SHORT {
             print!(".")
         } else if morse_value == LONG {
