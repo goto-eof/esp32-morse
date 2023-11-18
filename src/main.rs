b0cd216efc59e1c0d696e96ce6c8baef5eae0cb2
Andrei Dodu <andrei.dodu@hotmail.it>
2023-11-18 13:58:07 +0100

    feature: first version

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use anyhow::Ok;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+mod morse_service;
+use morse_service::translate;
+fn main() -> anyhow::Result<()> {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+
+    let string: &str = "Hello World";
+    let result = translate(string);
+    if result.is_err() {
+        println!("{}", result.err().unwrap());
+        return Ok(());
+    }
+    let morse: Vec<u32> = result.unwrap();
+    loop {
+        for morse_value in morse.clone() {
+            led.set_high()?;
+            FreeRtos::delay_ms(morse_value);
+            led.set_low()?;
+        }
+    }
+}
---
+++ src/morse_service.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+const SHORT: u32 = 100;
+const LONG: u32 = 300;
+const PAUSE: u32 = 500;
+const END: u32 = 1000;
+
+pub fn translate(string: &str) -> Result<Vec<u32>, String> {
+    let translation = retrieve_translation();
+
+    let mut morse_result: Vec<u32> = Vec::new();
+    for character in string.to_uppercase().chars() {
+        let morse_character_result = translation.get(&character);
+        if morse_character_result.is_none() {
+            let err_message = format!(
+                "Invalid character: could not translate the character: {}",
+                character
+            );
+            return Err(err_message);
+        }
+        let morse = &mut morse_character_result.unwrap().to_owned();
+        morse_result.append(morse);
+        morse_result.append(&mut vec![PAUSE])
+    }
+    morse_result.append(&mut vec![END]);
+    return Ok(morse_result);
+}
+
+fn retrieve_translation() -> HashMap<char, Vec<u32>> {
+    let translation: HashMap<char, Vec<u32>> = HashMap::from([
+        ('A', vec![SHORT, LONG]),
+        ('B', vec![LONG, SHORT, SHORT, SHORT]),
+        ('C', vec![LONG, SHORT, LONG, SHORT]),
+        ('D', vec![LONG, SHORT, SHORT]),
+        ('E', vec![SHORT]),
+        ('F', vec![SHORT, SHORT, LONG, SHORT]),
+        ('G', vec![LONG, LONG, SHORT]),
+        ('H', vec![SHORT, SHORT, SHORT, SHORT]),
+        ('I', vec![SHORT, SHORT]),
+        ('J', vec![SHORT, LONG, LONG, LONG]),
+        ('K', vec![LONG, SHORT, LONG]),
+        ('L', vec![SHORT, LONG, SHORT, SHORT]),
+        ('M', vec![LONG, LONG]),
+        ('N', vec![LONG, SHORT]),
+        ('O', vec![LONG, LONG, LONG]),
+        ('P', vec![SHORT, LONG, LONG, SHORT]),
+        ('Q', vec![LONG, LONG, SHORT, LONG]),
+        ('R', vec![SHORT, LONG, SHORT]),
+        ('S', vec![SHORT, SHORT, SHORT]),
+        ('T', vec![LONG]),
+        ('U', vec![SHORT, SHORT, LONG]),
+        ('V', vec![SHORT, SHORT, SHORT, LONG]),
+        ('U', vec![SHORT, LONG, LONG]),
+        ('X', vec![LONG, SHORT, SHORT, LONG]),
+        ('Y', vec![LONG, SHORT, LONG, LONG]),
+        ('Z', vec![LONG, LONG, SHORT, SHORT]),
+        ('1', vec![SHORT, LONG, LONG, LONG, LONG]),
+        ('2', vec![SHORT, SHORT, LONG, LONG, LONG]),
+        ('3', vec![SHORT, SHORT, SHORT, LONG, LONG]),
+        ('4', vec![SHORT, SHORT, SHORT, SHORT, LONG]),
+        ('5', vec![SHORT, SHORT, SHORT, SHORT, SHORT]),
+        ('6', vec![LONG, SHORT, SHORT, SHORT, SHORT]),
+        ('7', vec![LONG, LONG, SHORT, SHORT, SHORT]),
+        ('8', vec![LONG, LONG, LONG, SHORT, SHORT]),
+        ('9', vec![LONG, LONG, LONG, LONG, SHORT]),
+        ('0', vec![LONG, LONG, LONG, LONG, LONG]),
+        (' ', vec![PAUSE, PAUSE]),
+    ]);
+    translation
+}
